0458b8ee31fd7f93086261c7a6b1a1cb03c77b92
Delia Bouhan <deliabouhan10@gmail.com>
2019-12-06 21:59:14 -0600

    working FF app

---
+++ README.md
@@ -0,0 +1,13 @@
+### FRIEND FINDER
+
+## Setup
+- Clone this repo to your local directory.
+Navigate to your repo in your CLI and enter: npm install
+
+This will install the necessary NPM packages needed to run this application
+
+## Overview
+
+The home page qives information about the app.
+
+Navigate to the survey page, choose an answer to each question, click the submit button and meet your friend match!
---
+++ app/data/friends.js
@@ -0,0 +1,26 @@
+var friends = [
+    {
+    name: "Harry Potter",
+    photo: "https://timedotcom.files.wordpress.com/2014/07/301386_full1.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    },
+    {
+    name:"Ron Weasley",
+    photo:"https://image.insider.com/568d4264dd0895a83d8b45eb?width=1048&format=jpeg",
+    scores: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1],
+    },
+
+    {
+    name: "Hermoine Granger",
+    photo: "https://www.nypl.org/sites/default/files/hermione.jpg",
+    scores: [3, 5, 1, 2, 3, 5, 1, 4, 2, 3]
+    },
+
+    {
+    name: "Luna Lovegood",
+    photo: "https://images.ctfassets.net/usf1vwtuqyxm/t6GVMDanqSKGOKaCWi8oi/74b6816d9f913623419b98048ec87d25/LunaLovegood_WB_F5_LunaLovegoodPromoCloseUp_Promo_080615_Port.jpg",
+    scores: [4, 2, 5, 1, 3, 3, 2, 5, 1, 2]
+    },
+]
+
+module.exports = friends;
